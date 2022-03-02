2df1f0b99f351b0b46a6bd2fc78d7a9f1a75b2ab
Taylor Bedrosian <taylorbedrosian@MacBook-Pro.local>
2022-03-01 22:34:30 -0800

    #working

--- matmul.s
+++ matmul.s
@@ -13,15 +13,15 @@
 	.global matmul
 
 matmul:
-	stp    x29, x30, [sp, -64]!
+	stp    x29, x30, [sp, -96]!
 	add    x29, sp, 0
-   mov     x9, #0 // i
-   mov     x10, #0 // j
-   mov     x11, #0 // k
-   mov     x12, #0 // sum
-   mov     x13, #0 // a matrix offset
-   mov     x14, #0 // b matrix offset
-   mov     x15, #0 // c matrix offset
+   mov    x9, #0 // i
+   mov    x10, #0 // j
+   mov    x11, #0 // k
+   mov    x12, #0 // sum
+   mov    x13, #0 // a matrix offset
+   mov    x14, #0 // b matrix offset
+   mov    x15, #0 // c matrix offset
 
    iloopstart:
       cmp   x9, x3 
@@ -31,14 +31,17 @@ matmul:
          cmp   x10, x5 // checking if j < wB
          b.eq  jloopend 
          mov    x11, #0 // k = 0
-         mov    x12, #0 // sum = 0            
+         str    x11, [x29, 72]
+         mov    x12, #0 // sum = 0  
+         str   x12, [x29, 64]          
          stp   x0, x1, [x29, 16] // save parameter regesters to stack
          stp   x2, x3, [x29, 32]
          stp   x4, x5, [x29, 48]
+         stp   x9, x10, [x29, 80]
          kloopstart:
             cmp   x11, x4 // checking if k < wA
             b.eq  kloopend
-
+            
             /* sum += A[i * wA + k] * B[k * wB + j]; */
             /* calculating a offset */
             mov   x0, x9 
@@ -46,25 +49,38 @@ matmul:
             bl    intmul
             mov   x2, x11
             bl    intadd
+            mov   x2, #4
+            bl    intmul 
             mov   x13, x0   /* set a offset */
             /* calculate b offset */
             mov   x0, x11 /* k */
-            mov   x2, x5  /* wB */
+            ldr   x2, [x29, 48]  /* wB */
             bl    intmul
             mov   x2, x10 /* j */
             bl    intadd
+            mov   x2, #4
+            bl    intmul
             mov   x14, x0 /* set b offset */
             /* now add to sum */
-            ldrb  x0, [x1, x13]
-            ldrb  x2, [x2, x14]
+            ldp   x1, x2, [x29, 24]
+            ldrsw   x0, [x1, x13]
+            ldrsw   x2, [x2, x14]
             bl    intmul  /* do the matrix mult */
+            ldr   x12, [x29, 64]
             mov   x2, x12 
             bl    intadd  /* add to the running total */
             mov   x12, x0 /* set the result to the new running total */
+            str   x12, [x29, 64]
+            ldr   x11, [x29, 72]
             mov   x0, x11
             mov   x2, #1
             bl    intadd
             mov   x11, x0 /* increment k by one, update k to new value */
+            str   x11, [x29, 72]
+            ldp   x0, x1, [x29, 16]
+            ldp   x2, x3, [x29, 32]
+            ldp   x4, x5, [x29, 48] /* restore parameter registers */
+            ldp   x9, x10, [x29, 80]
             bl    kloopstart
 
          kloopend:
@@ -73,11 +89,13 @@ matmul:
             bl    intmul  /* i * wB */
             mov   x2, x10 
             bl    intadd  /* + j */
+            mov   x2, #4
+            bl    intmul
             mov   x15, x0 /* copy c offset to x15 */
             ldp   x0, x1, [x29, 16]
             ldp   x2, x3, [x29, 32]
             ldp   x4, x5, [x29, 48] /* restore parameter registers */
-            strb   x12, [x0, x15] /* push sum to c ptr at offset */
+            str   x12, [x0, x15] /* push sum to c ptr at offset */
             stp   x0, x1, [x29, 16] // save parameter regesters to stack
             stp   x2, x3, [x29, 32]
             stp   x4, x5, [x29, 48]
@@ -102,7 +120,7 @@ matmul:
          ldp   x4, x5, [x29, 48] /* restore parameter registers */
          bl    iloopstart
    end:
-      ldp  x29, x30, [sp], 64
+      ldp  x29, x30, [sp], 96
       ret
 ////////////////////////////////////////////////////////////////////////////////
 // You're implementing the following function in ARM Assembly
